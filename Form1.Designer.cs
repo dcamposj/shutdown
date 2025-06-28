7f62bffcb77f55a84cd54e88a02186060b1f917d
dcamposj <diegocj92@gmail.com>
2025-06-28 14:15:01 -0600

    csproj new config

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace shutdownsharp
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHours = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite el tiempo deseado (en horas):";
+            // 
+            // txtHours
+            // 
+            this.txtHours.Location = new System.Drawing.Point(176, 12);
+            this.txtHours.Name = "txtHours";
+            this.txtHours.Size = new System.Drawing.Size(100, 20);
+            this.txtHours.TabIndex = 1;
+            this.txtHours.Text = "0";
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(105, 38);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 2;
+            this.btnRun.Text = "Run Script";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.BtnRun_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 71);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.txtHours);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Shutdown Timer Launcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHours;
+        private System.Windows.Forms.Button btnRun;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+using System.Diagnostics;
+
+namespace shutdownsharp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnRun_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtHours.Text, out double hours) && hours > 0)
+            {
+                // Create the modified PowerShell script
+                string script =
+                $@"$timerHours = {hours.ToString(System.Globalization.CultureInfo.InvariantCulture)}
+                $timerSeconds = [math]::Round($timerHours * 3600)
+                Write-Host ""Dejar esta ventana abierta la PC se apaga en $timerHours horas...""
+                Start-Sleep -Seconds $timerSeconds
+                Stop-Computer -Force";
+
+                // Get temp file path
+                string tempScriptPath = Path.Combine(Path.GetTempPath(), "ShutdownTimer.ps1");
+
+                // Write the script to file
+                File.WriteAllText(tempScriptPath, script);
+
+                // Create process start info
+                ProcessStartInfo psi = new()
+                {
+                    FileName = "powershell.exe",
+                    Arguments = $"-NoExit -ExecutionPolicy Bypass -File \"{tempScriptPath}\"",
+                    UseShellExecute = true
+                };
+
+                // Start the process
+                Process.Start(psi);
+
+                // Close the launcher
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Por favor digite un valor válido mayor a 0.", "Error de entrada", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
